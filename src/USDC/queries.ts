635601a0cc55ea11a455b347402a5d9777944efb
Samuel Roy <samuelrobinroy@gmail.com>
2021-06-01 11:40:43 +0530

    reward logic

---
+++ src/USDC/queries.ts
@@ -0,0 +1,27 @@
+import shibaSwapData from '@shibaswap/shibaswap-data-snoop'
+
+type Info = shibaSwapData.topdog.Info;
+
+type Pools = shibaSwapData.topdog.Pool[];
+
+type Claims = shibaSwapData.vesting.User[];
+
+type Users = shibaSwapData.topdog.User[];
+
+export default {
+    async info(block_number: number): Promise<Info> {   
+        return await shibaSwapData.topdog.info({block: block_number});
+    },
+
+    async pools(block_number: number): Promise<Pools> {
+        return await shibaSwapData.topdog.pools({block: block_number});
+    },
+    
+    async claims(block_number?: number): Promise<Claims> {
+        return await shibaSwapData.vesting.users({block: block_number})
+    },
+
+    async users(block_number: number): Promise<Users> {
+        return await shibaSwapData.topdog.users({block: block_number});
+    }
+}
