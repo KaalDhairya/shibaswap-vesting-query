7a43ed2d02c0dca311313f05a987d2edb70dcbe3
Shadow Hunter <shadow.hunter00121@gmail.com>
2021-07-30 01:57:31 +0530

    xFund test

---
+++ src/xFund/index.ts
@@ -0,0 +1,58 @@
+import shibaSwapData from '@shibaswap/shibaswap-data-snoop';
+import { parseBalanceMap } from '../parse-balance-map'
+import queries from './queries';
+import { finalize } from '../rewardCalculation'
+import { Options } from '../types'
+import { TOPDOG_COLLECTION } from '../Database/constants';
+
+
+export default async function getDistribution(options: Options) {
+    options.startBlock = options.startBlock;
+    options.claimBlock = options.claimBlock ?? (await shibaSwapData.blocks.latestBlock()).number;
+    console.log("**************************")
+    console.log(options.claimBlock);
+    // Fetch the data and redirect the addresses right away
+    // const data = redirect(await fetchData(options.startBlock, options.endBlock, options.claimBlock));
+    // Rewars claimed by the users till now 
+
+
+    const VESTED_AMOUNT = 0;
+    const INPUT_DECIMAL = 1e18;
+    const OUTPUT_DECIMAL = 1e9;
+    const POOL = 8;
+    const REWARD_AMOUNT = 5000;   //33% of total rewards
+    const LOCK_PERCENT = 67;
+    const UNLOCK_PERCENT = 33;
+    const WEEK = 1;
+    const REWARD_WEEK = 0;
+    const REWARD_TOKEN = "XFUND"
+    const CONTRACT = "TopDog"
+
+
+    // const claims = await queries.claims(options.claimBlock);
+    const final = await finalize(
+        options.startBlock, 
+        options.endBlock,
+        REWARD_AMOUNT,
+        WEEK,
+        REWARD_WEEK,
+        REWARD_TOKEN,
+        CONTRACT,
+        POOL,
+        UNLOCK_PERCENT,
+        LOCK_PERCENT,
+        INPUT_DECIMAL,
+        OUTPUT_DECIMAL,
+        [],
+        TOPDOG_COLLECTION
+        );
+
+    // console.log(final.users)
+
+    return {
+        amounts: final.users,
+        blacklisted: final.blacklisted,
+        merkle: parseBalanceMap(final.users),
+        lockInfo: final.lockInfo
+    };
+}
---
+++ src/xFund/standalone.ts
@@ -0,0 +1,67 @@
+import { Command } from "commander";
+import fs from "fs";
+import shibaSwapData from '@shibaswap/shibaswap-data-snoop';
+
+import getDistribution from './index';
+import { VESTING_START } from "../constants";
+
+const program = new Command();
+
+type Options = {
+    startBlock: number,
+    endBlock: number,
+    claimBlock: number
+};
+
+program
+    .option('-s, --startBlock <number>')
+    .option('-e, --endBlock <number>')
+    .option('-c, --claimBlock <number>')
+
+program.parse(process.argv);
+
+main();
+
+async function main() {
+    const options: Options = {
+        startBlock: Number(program.opts().startBlock ?? VESTING_START),
+        endBlock: Number(program.opts().endBlock),
+        claimBlock: Number(program.opts().claimBlock ?? await shibaSwapData.utils.timestampToBlock(Date.now()))
+    }
+
+    const distribution = await getDistribution(options);
+
+    console.log("Generating files")
+
+    if(!fs.existsSync('./outputs/WBTC')) {
+        fs.mkdirSync('./outputs/WBTC', { recursive: true})
+    }
+
+    fs.writeFileSync(
+        `./outputs/WBTC/amounts-${options.startBlock}-${options.endBlock}.json`,//-${options.claimBlock}}`, - will enable when subgraph switches to mainnet
+        JSON.stringify(
+            distribution.amounts, null, 1
+        )
+    );
+
+    fs.writeFileSync(
+        `./outputs/WBTC/blacklisted-${options.startBlock}-${options.endBlock}.json`,//-${options.claimBlock}}`, - will enable when subgraph switches to mainnet
+        JSON.stringify(
+            distribution.blacklisted, null, 1
+        )
+    );
+
+    fs.writeFileSync(
+        `./outputs/WBTC/merkle-${options.startBlock}-${options.endBlock}.json`,//-${options.claimBlock}}`, - will enable when subgraph switches to mainnet
+        JSON.stringify(
+            distribution.merkle, null, 1
+        )
+    )
+
+    fs.writeFileSync(
+        `./outputs/WBTC/lockInfo-${options.startBlock}-${options.endBlock}.json`,//-${options.claimBlock}}`, - will enable when subgraph switches to mainnet
+        JSON.stringify(
+            distribution.lockInfo, null, 1
+        )
+    )
+};
