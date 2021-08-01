41c11dc4eaff43ca408dd8ea5f57866f71d8c5df
Shadow Hunter <shadow.hunter00121@gmail.com>
2021-08-01 22:18:57 +0530

    log claims length

--- src/BuryShibBone/index.ts
+++ src/BuryShibBone/index.ts
@@ -36,6 +36,7 @@ export default async function getDistribution(options: Options) {
 
 
     const claims = await queries.claims(options.claimBlock);
+    console.log(claims.length)
     const final = await finalize(
         options.startBlock, 
         options.endBlock,
