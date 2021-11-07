02aef26b1ceafc423aad41e0bc77901d97c8f9d8
Shadow Hunter <shadow.hunter00121@gmail.com>
2021-11-08 00:50:47 +0530

    minor

--- src/Database/constants.ts
+++ src/Database/constants.ts
@@ -1,7 +1,7 @@
 export const DB = "vesting"
 export const WEEKLY_REWARD_INFO_COLLECTION = "rewardscollections"
 export const USER_INFO_COLLECTION = "user_info_v2"
-export const URL = "mongodb://127.0.0.1:27017"
+export const URL = "mongodb://kaal:password@127.0.0.1:27017/vesting?authSource=admin"
 export const BURRY_BONE_COLLECTION = "burybonecollections"
 export const BURRY_LEASH_COLLECTION = "buryleashcollections"
 export const BURRY_SHIB_COLLECTION = "buryshibcollections"
