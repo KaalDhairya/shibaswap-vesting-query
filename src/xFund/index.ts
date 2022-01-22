5d73b1fddc683e77230039d46ff3dee5db3e8572
Shadow Hunter <shadow.hunter00121@gmail.com>
2022-01-22 21:40:14 +0530

    xfund amount change

--- src/xFund/index.ts
+++ src/xFund/index.ts
@@ -19,7 +19,7 @@ export default async function getDistribution(options: Options) {
 
     const WEEK = 14;
     const REWARD_WEEK = 0;
-    const REWARD_AMOUNT = 16.5;   //33% of total rewards
+    const REWARD_AMOUNT = 10;
 
 
 
